b66467ad68ded76386c549ee232079287a467530
xyzeez <106898953+xyzeez@users.noreply.github.com>
2024-06-09 02:39:24 +0100

    🎉 start(app): initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// Pages
+import Home from './pages/Home';
+import Product from './pages/Products';
+import Pricing from './pages/Pricing';
+import PageNotFound from './pages/PageNotFound';
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
